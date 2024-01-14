0cf394e3b3eb89a6a730fc437edbb670053d062e
Previo02 <alessandro.previati@studio.unibo.it>
2024-01-14 12:47:51 +0100

    Second commit: - Better CSS version of profile, added js function to complete with the database. - Modified structure of profile, better button group and linear button look. - TODO js function, maybe background. Graphic look is bland, todo better.

--- profile/profile.js
+++ profile/profile.js
@@ -1,7 +1,15 @@
-function immagineProfilo() {
+function caricaDatiProfilo() {
+    const datiProfilo = ottieniDatiProfiloDalDatabase(); /*TODO*/
 
+    document.getElementById('nomeUtente').innerText = datiProfilo.nomeUtente;
+    document.getElementById('dataCreazione').innerText = "Member since: " + datiProfilo.dataCreazione;
+    document.getElementById('immagineProfilo').src = datiProfilo.urlImmagineProfilo;
 }
 
-function nomeUtente() {
+function ottieniDatiProfiloDalDatabase() {
     
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    caricaDatiProfilo();
+});
