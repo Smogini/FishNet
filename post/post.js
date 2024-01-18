bf4498bb3be049f8b5e79c402f27dd13fe4a474f
Previo02 <alessandro.previati@studio.unibo.it>
2024-01-18 11:37:16 +0100

    -added Post -todo finish post

---
+++ post/post.js
@@ -0,0 +1,22 @@
+function initMap() {
+    var map = L.map('map').setView([0, 0], 2);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© OpenStreetMap contributors'
+    }).addTo(map);
+
+    var marker = L.marker([0, 0], { draggable: true }).addTo(map);
+
+    marker.on('dragend', function (e) {
+        var latlng = marker.getLatLng();
+        document.getElementById('location').value = latlng.lat + ', ' + latlng.lng;
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    initMap();
+});
+
+function post() {
+    /*TODO*/
+}
